1ec127b192d9b6f17febe0afb8f24e818a9b2074
Ilie1985 <ilie.marian32@yahoo.com>
2023-04-05 14:16:56 +0100

    create sidebar comp

---
+++ components/Sidebar.js
@@ -0,0 +1,62 @@
+import Image from "next/image";
+import React from "react";
+import SidebarMenuItem from "./SidebarMenuItem";
+import { HomeIcon } from "@heroicons/react/solid";
+import {
+  BellIcon,
+  BookmarkIcon,
+  ClipboardIcon,
+  DotsCircleHorizontalIcon,
+  DotsHorizontalIcon,
+  HashtagIcon,
+  InboxIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+
+const Sidebar = () => {
+  return (
+    <div className=" sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24">
+      {/* Twitter logo */}
+      <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/1200px-Twitter-logo.svg.png"
+          alt="twitter logo"
+          width="50"
+          height="50"
+        ></Image>
+      </div>
+      {/* Menu */}
+      <div className="mt-4 mb-2.5 xl:items-start">
+        <SidebarMenuItem text="Home" Icon={HomeIcon} active />
+        <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
+        <SidebarMenuItem text="Notifications" Icon={BellIcon} />
+        <SidebarMenuItem text="Messages" Icon={InboxIcon} />
+        <SidebarMenuItem text="BookMark" Icon={BookmarkIcon} />
+        <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
+        <SidebarMenuItem text="Profile" Icon={UserIcon} />
+        <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
+      </div>
+
+      {/* Button */}
+      <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
+        Tweet
+      </button>
+
+      {/*Mini-profile */}
+      <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto ">
+        <img
+          src="https://media.licdn.com/dms/image/C4D03AQHlMq3vFRy6DQ/profile-displayphoto-shrink_800_800/0/1645124997754?e=2147483647&v=beta&t=lCWYwD_G_je1XkeucVLOEDFyBTUprs0V46AFOEJUDo0"
+          alt="user-image"
+          className="  h-10 w-10 rounded-full xl:mr-2 "
+        />
+        <div className="leading-5 hidden xl:inline">
+          <h4 className="font-bold ">Ilie Marian</h4>
+          <p className="text-gray-500 s">@marianilie</p>
+        </div>
+        <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
