063c042974502038c6480d5ca533cee1041f349f
Ilie1985 <ilie.marian32@yahoo.com>
2023-04-05 17:54:56 +0100

    add post section of Feed comp

--- components/Feed.js
+++ components/Feed.js
@@ -1,8 +1,31 @@
 import React from "react";
 import { SparklesIcon } from "@heroicons/react/outline";
 import Input from "./Input";
+import Post from "./Post";
 
 const Feed = () => {
+  const posts = [
+    {
+      id: "1",
+      name: "Marian Ilie",
+      username: "marianilie",
+      userImg:
+        "https://media.licdn.com/dms/image/C4D03AQHlMq3vFRy6DQ/profile-displayphoto-shrink_800_800/0/1645124997754?e=2147483647&v=beta&t=lCWYwD_G_je1XkeucVLOEDFyBTUprs0V46AFOEJUDo0",
+      img: "https://images.unsplash.com/photo-1680263547745-4e0555920ea2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+      text: "Nice view!",
+      timestamp: "2hours ago",
+    },
+    {
+      id: "2",
+      name: "Mario",
+      username: "mario",
+      userImg:
+        "https://media.licdn.com/dms/image/C4D03AQHlMq3vFRy6DQ/profile-displayphoto-shrink_800_800/0/1645124997754?e=2147483647&v=beta&t=lCWYwD_G_je1XkeucVLOEDFyBTUprs0V46AFOEJUDo0",
+      img: "https://plus.unsplash.com/premium_photo-1675237625886-7529b3b0c1cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0NHx8fGVufDB8fHx8&auto=format&fit=crop&w=1200&q=60",
+      text: "Yummy Yum!",
+      timestamp: "2.5 hours ago",
+    },
+  ];
   return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
       <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
@@ -11,7 +34,10 @@ const Feed = () => {
           <SparklesIcon className="h-5" />
         </div>
       </div>
-      <Input/>
+      <Input />
+      {posts.map((post) => {
+        return <Post key={post.id} post={post} />;
+      })}
     </div>
   );
 };
---
+++ components/Post.js
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  ChartBarIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  HeartIcon,
+  ShareIcon,
+  TrashIcon,
+} from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+
+const Post = ({ post }) => {
+  return (
+    <div className="flex p-3 cursor-pointer border-b border-gray-200 ">
+      {/*User Image */}
+      <img
+        src={post.userImg}
+        alt="user-image"
+        className="h-11 w-11 rounded-full mr-4"
+      />
+
+      {/* Right Side */}
+      <div className="">
+        {/* Header */}
+        <div className="flex items-center justify-between ">
+          {/*  Post user info */}
+          <div className="flex space-x-1 items-center whitespace-nowrap">
+            <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline ">
+              {post.name}
+            </h4>
+            <span className="text-sm sm:text-[15px] sm:mt-1">
+              @{post.username}-{" "}
+            </span>
+            <span className="text-sm sm:text-[15px] hover:underline sm:mt-1">
+              {post.timestamp}
+            </span>
+          </div>
+          {/* Dot icon */}
+          <DotsHorizontalIcon className="h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2" />
+        </div>
+
+        {/* Post text */}
+        <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2 ">{post.text}</p>
+
+        {/*Post Image */}
+        <img src={post.img} alt="posted images" className="rounded-2xl mr-2"/>
+
+        {/* Icons */}
+        <div className="flex justify-between text-gray-500 p-2">
+        <ChatIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+        <TrashIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
+        <HeartIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
+        <ShareIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+        <ChartBarIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
