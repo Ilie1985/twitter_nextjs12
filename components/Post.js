c3a40129baa60cecdca51996ada8fc48e8772e30
Ilie1985 <ilie.marian32@yahoo.com>
2023-04-12 12:58:34 +0100

    add ike functionality to the post

--- components/Post.js
+++ components/Post.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   ChartBarIcon,
   ChatIcon,
@@ -9,8 +9,50 @@ import {
 } from "@heroicons/react/outline";
 import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
 import Moment from "react-moment";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  setDoc,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import { signIn, useSession } from "next-auth/react";
 
 const Post = ({ post }) => {
+  const [likes, setLikes] = useState([]);
+  const [hasLikes, setHasLikes] = useState(false);
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(db, "posts", post.id, "likes"),
+      (snapshot) => setLikes(snapshot.docs)
+    );
+  }, [db]);
+
+  useEffect(() => {
+    setHasLikes(
+      likes.findIndex((like) => {
+        return like.id === session?.user.uid;
+      }) !== -1
+    );
+  }, [likes]);
+
+  const likePost = async () => {
+    if (session) {
+      if (hasLikes) {
+        await deleteDoc(doc(db, "posts", post.id, "likes", session?.user.uid));
+      } else {
+        await setDoc(doc(db, "posts", post.id, "likes", session?.user.uid), {
+          username: session.user.username,
+        });
+      }
+    }else{
+      signIn("/")
+    }
+  };
+
   return (
     <div className="flex p-3 cursor-pointer border-b border-gray-200 ">
       {/*User Image */}
@@ -33,7 +75,7 @@ const Post = ({ post }) => {
               @{post.data().username}-{" "}
             </span>
             <span className="text-sm sm:text-[15px] hover:underline sm:mt-1">
-              <Moment fromNow>{post?.timestamp?.toDate()}</Moment>
+              <Moment fromNow>{post?.data().timestamp?.toDate()}</Moment>
             </span>
           </div>
           {/* Dot icon */}
@@ -56,7 +98,29 @@ const Post = ({ post }) => {
         <div className="flex justify-between text-gray-500 p-2">
           <ChatIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
           <TrashIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
-          <HeartIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
+
+          <div className="flex items-center">
+            {hasLikes ? (
+              <HeartIconFilled
+                onClick={likePost}
+                className="h-9 w-9 hoverEffect p-2 text-red-600 hover:bg-red-100"
+              />
+            ) : (
+              <HeartIcon
+                onClick={likePost}
+                className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100"
+              />
+            )}
+
+            {likes.length > 0 && (
+              <span
+                className={`${hasLikes && "text-red-600"} text-sm select-none`}
+              >
+                {likes.length}
+              </span>
+            )}
+          </div>
+
           <ShareIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
           <ChartBarIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
         </div>
