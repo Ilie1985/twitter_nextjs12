83911ab226978d3506e224aeb28ad3fb81c7a39c
Ilie1985 <ilie.marian32@yahoo.com>
2023-04-12 12:11:14 +0100

    get post data from firestore show it in Post section

--- components/Post.js
+++ components/Post.js
@@ -8,13 +8,14 @@ import {
   TrashIcon,
 } from "@heroicons/react/outline";
 import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import Moment from "react-moment";
 
 const Post = ({ post }) => {
   return (
     <div className="flex p-3 cursor-pointer border-b border-gray-200 ">
       {/*User Image */}
       <img
-        src={post.userImg}
+        src={post.data().userImg}
         alt="user-image"
         className="h-11 w-11 rounded-full mr-4"
       />
@@ -26,13 +27,13 @@ const Post = ({ post }) => {
           {/*  Post user info */}
           <div className="flex space-x-1 items-center whitespace-nowrap">
             <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline ">
-              {post.name}
+              {post.data().name}
             </h4>
             <span className="text-sm sm:text-[15px] sm:mt-1">
-              @{post.username}-{" "}
+              @{post.data().username}-{" "}
             </span>
             <span className="text-sm sm:text-[15px] hover:underline sm:mt-1">
-              {post.timestamp}
+              <Moment fromNow>{post?.timestamp?.toDate()}</Moment>
             </span>
           </div>
           {/* Dot icon */}
@@ -40,19 +41,24 @@ const Post = ({ post }) => {
         </div>
 
         {/* Post text */}
-        <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2 ">{post.text}</p>
+        <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2 ">
+          {post.data().text}
+        </p>
 
         {/*Post Image */}
-        <img src={post.img} alt="posted images" className="rounded-2xl mr-2"/>
+        <img
+          src={post.data().image}
+          alt="posted images"
+          className="rounded-2xl mr-2"
+        />
 
         {/* Icons */}
         <div className="flex justify-between text-gray-500 p-2">
-        <ChatIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
-        <TrashIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
-        <HeartIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
-        <ShareIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
-        <ChartBarIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
-
+          <ChatIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+          <TrashIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
+          <HeartIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
+          <ShareIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+          <ChartBarIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
         </div>
       </div>
     </div>
